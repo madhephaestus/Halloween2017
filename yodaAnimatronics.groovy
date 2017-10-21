cc9bbc09142899ae3c32bc56c009e16f90238590
Kevin Harrington <mad.hephaestus@gmail.com>
2017-10-21 16:51:24 -0400

    head scalled to fit inside Yoda

--- yodaAnimatronics.groovy
+++ yodaAnimatronics.groovy
@@ -8,19 +8,19 @@ LengthParameter thickness 		= new LengthParameter(	"Material Thickness",
 												3.2,
 												[10,1])
 LengthParameter headDiameter 		= new LengthParameter(	"Head Dimeter",
-												65,
+												80,
 												[200,140])
-LengthParameter snoutLen 		= new LengthParameter("Snout Length",50,[headDiameter.getMM()*2,headDiameter.getMM()/2])
-LengthParameter jawHeight 		= new LengthParameter("Jaw Height",15,[200,10])
+LengthParameter snoutLen 		= new LengthParameter("Snout Length",65,[headDiameter.getMM()*2,headDiameter.getMM()/2])
+LengthParameter jawHeight 		= new LengthParameter("Jaw Height",20,[200,10])
 LengthParameter JawSideWidth 		= new LengthParameter("Jaw Side Width",20,[40,10])
 LengthParameter boltDiam 		= new LengthParameter("Bolt Diameter",3.0,[8,2])
 LengthParameter boltLength		= new LengthParameter("Bolt Length",10,[18,10])
 LengthParameter nutDiam 		 	= new LengthParameter("Nut Diameter",5.42,[10,3])
 LengthParameter nutThick 		= new LengthParameter("Nut Thickness",2.4,[10,3])
-LengthParameter upperHeadDiam 	= new LengthParameter("Upper Head Height",20,[300,0])
-LengthParameter leyeDiam 		= new LengthParameter("Left Eye Diameter",20,[headDiameter.getMM()/2,56])
-LengthParameter reyeDiam 		= new LengthParameter("Right Eye Diameter",20,[headDiameter.getMM()/2,56])
-LengthParameter eyeCenter 		= new LengthParameter("Eye Center Distance",30,[headDiameter.getMM(),leyeDiam.getMM()*1.5])
+LengthParameter upperHeadDiam 	= new LengthParameter("Upper Head Height",50,[300,0])
+LengthParameter leyeDiam 		= new LengthParameter("Left Eye Diameter",27,[headDiameter.getMM()/2,56])
+LengthParameter reyeDiam 		= new LengthParameter("Right Eye Diameter",27,[headDiameter.getMM()/2,56])
+LengthParameter eyeCenter 		= new LengthParameter("Eye Center Distance",55,[headDiameter.getMM(),leyeDiam.getMM()*1.5])
 LengthParameter ballJointPin		= new LengthParameter("Ball Joint Pin Size",8,[50,8])
 LengthParameter centerOfBall 		= new LengthParameter("Center Of Ball",18.5,[50,8])
 LengthParameter printerOffset		= new LengthParameter("printerOffset",0.5,[2,0.001])
@@ -59,15 +59,16 @@ File yodaFile = ScriptingEngine.fileFromGit(
 	"Yoda-SuperLite.stl");
 CSG cutter = new Cube(headDiameter.getMM()*4).toCSG()
 // Load the .CSG from the disk and cache it in memory
+double scale = 1.6
 CSG yoda  = Vitamins.get(yodaFile)
 			.rotz(-55)
-			.scale(1.75)
+			.scale(scale)
 			.difference(cutter
 					.toYMin()
 					.toZMin()
 				)
-			.movez(-38+jawHeight.getMM())
-			.movex(-(75/2) + headDiameter.getMM()/2)
+			.movez(-82+jawHeight.getMM())
+			.movex(-(65) + headDiameter.getMM()/2)
 headParts.add(yoda )
 
 return headParts
